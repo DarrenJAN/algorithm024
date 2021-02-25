4d5537bb71678fe56f804aa36d7c306170533683
ZZ <8413624+CDCZZ@user.noreply.gitee.com>
2021-02-25 21:10:45 +0800

    2.25

---
+++ Week_02/src/q429_levelOrder.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class q429_levelOrder {
+}
---
+++ Week_02/src/q590_postorderTraversal.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class postorderTraversal {
+    public List<Integer> postorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+
+    }
+
+    public void helper(Node root, List<Integer> result)
+    {
+        
+    }
+}
