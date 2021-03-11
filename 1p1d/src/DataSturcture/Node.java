7293b8096cb2ab5a8a05858b9f49c546044a5b67
ZZ <8413624+CDCZZ@user.noreply.gitee.com>
2021-03-11 16:44:42 +0800

    3.10

---
+++ 1p1d/src/DataSturcture/Node.java
@@ -0,0 +1,2 @@
+package DataSturcture;public class Node {
+}
---
+++ 1p1d/src/day40_maxSumSubmatrix.java
@@ -0,0 +1,36 @@
+public class q363_maxSumSubmatrix {
+
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int rows = matrix.length, cols = matrix[0].length, max = Integer.MIN_VALUE;
+        // O(cols ^ 2 * rows)
+        for (int l = 0; l < cols; l++) { // 枚举左边界
+            int[] rowSum = new int[rows]; // 左边界改变才算区域的重新开始
+            for (int r = l; r < cols; r++) { // 枚举右边界
+                for (int i = 0; i < rows; i++) { // 按每一行累计到 dp
+                    rowSum[i] += matrix[i][r];
+                }
+
+                // 求 rowSum 连续子数组 的 和
+                // 和 尽量大，但不大于 k
+                max = Math.max(max, dpmax(rowSum, k));
+            }
+        }
+        return max;
+    }
+
+    // 在数组 arr 中，求不超过 k 的最大值
+    private int dpmax(int[] arr, int k) {
+        // O(rows ^ 2)
+        int max = Integer.MIN_VALUE;
+        for (int l = 0; l < arr.length; l++) {
+            int sum = 0;
+            for (int r = l; r < arr.length; r++) {
+                sum += arr[r];
+                if (sum > max && sum <= k) max = sum;
+            }
+        }
+        return max;
+    }
+
+    
+}
